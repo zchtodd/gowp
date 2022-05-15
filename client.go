094faea2f9226a0c09178745ce7c567f48198128
Zach Todd <zchtodd@gmail.com>
2022-05-15 19:46:41 +0000

    WIP

--- client.go
+++ client.go
@@ -1,44 +1,65 @@
 package main
 
 import (
+    "os"
     "fmt"
     "log"
     "flag"
+    "strings"
+    "bytes"
+    "encoding/json"
     "bufio"
     "path"
     "io/ioutil"
     "net/url"
     "net/http"
+    "github.com/mdp/qrterminal/v3"
     "github.com/gorilla/websocket"
 )
 
-func Register() string {
-   response, err := http.Get("http://corra.io:8080/register")
+type arrayFlags []string
+
+func (a *arrayFlags) String() string {
+   return ""
+}
+
+func (a *arrayFlags) Set(value string) error {
+   *a = append(*a, value)
+   return nil
+}
+
+type RegisterPayload struct {
+   Ports []string `json:"ports"`
+}
+
+func Register(proxyPassPorts arrayFlags) string {
+   payload := RegisterPayload{Ports: proxyPassPorts}
+   payloadBuf := new(bytes.Buffer)
+
+   json.NewEncoder(payloadBuf).Encode(payload)
+
+   request, err := http.NewRequest("POST", "http://corra.io:8080/register", payloadBuf)
+   if err != nil {
+       log.Fatal(err)
+   }
+
+   client := &http.Client{}
+   response, err := client.Do(request)
    if err != nil {
        log.Fatal(err)
    }
 
    body, err := ioutil.ReadAll(response.Body)
-
    if err != nil {
        log.Fatal(err)
    }
-   response.Body.Close()
 
+   response.Body.Close()
    return string(body)
 }
 
-func StartProxy(proxyPassURL *url.URL) {
-   subdomain := Register()
-
-   log.Printf("Proxy subdomain registered: %s\n", subdomain)
-
-   proxyServerURL, err := url.Parse(fmt.Sprintf("ws://%s.corra.io:8080/proxy", subdomain))
-   if err != nil {
-       log.Fatal(err)
-   }
-
-   c, _, err := websocket.DefaultDialer.Dial(proxyServerURL.String(), nil)
+func StartProxy(proxyHostURL *url.URL, proxyPassHost string) {
+   c, _, err := websocket.DefaultDialer.Dial(proxyHostURL.String(), nil)
    if err != nil {
        log.Fatal(err)
    }
@@ -58,7 +79,8 @@ func StartProxy(proxyPassURL *url.URL) {
            return
        }
 
-       u, err := url.Parse(proxyPassURL.String())
+       hostAndPort := strings.Split(request.Host, ":")
+       u, err := url.Parse(proxyPassHost + ":" + hostAndPort[1])
        if err != nil {
            log.Println(err)
            return
@@ -68,6 +90,8 @@ func StartProxy(proxyPassURL *url.URL) {
 
        request.RequestURI = ""
        request.URL = u
+
+       log.Printf("Sending proxy request: %s\n", u.String())
 
        client := http.Client{}
        response, err := client.Do(request)
@@ -87,13 +111,25 @@ func StartProxy(proxyPassURL *url.URL) {
 }
 
 func main() {
-   proxyPass := flag.String("proxy-pass", "", "The server that should receive requests from the proxy.")
+   var proxyPassPorts arrayFlags
+
+   proxyPassHost := flag.String("hostname", "", "The hostname that should receive requests from the proxy.")
+   flag.Var(&proxyPassPorts, "port", "The ports that proxy requests should be routed to.")
+
    flag.Parse()
 
-   proxyPassURL, err := url.Parse(*proxyPass)
+   proxyHostURL, err := url.Parse(fmt.Sprintf("ws://%s.corra.io:8080/proxy", Register(proxyPassPorts)))
    if err != nil {
        log.Fatal(err)
    }
 
-   StartProxy(proxyPassURL)
+   qrterminal.Generate("http://" + proxyHostURL.Host, qrterminal.L, os.Stdout)
+   fmt.Printf("\nYour proxy session has started!\n")
+   fmt.Printf("\nScan the QR code above or go to one of the following addresses: \n\n")
+
+   for _, proxyPassPort := range proxyPassPorts {
+       fmt.Printf("    https://%s:%s\n", proxyHostURL.Hostname(), proxyPassPort)
+   }
+
+   StartProxy(proxyHostURL, *proxyPassHost)
 }
--- server.go
+++ server.go
@@ -6,10 +6,15 @@ import (
     "bytes"
     "strings"
     "net/http"
+    "encoding/json"
     "github.com/gorilla/mux"
     "github.com/gorilla/websocket"
     "github.com/lithammer/shortuuid/v4"
 )
+
+type RegisterPayload struct {
+   Ports []string `json:"ports"`
+}
 
 type ProxyValue struct {
     Conn    *websocket.Conn
@@ -17,7 +22,9 @@ type ProxyValue struct {
 }
 
 type Proxy struct {
+    Router  *mux.Router
     Clients map[string]*ProxyValue
+    Ports   []string
 }
 
 type ProxyHandler func(*Proxy, http.ResponseWriter, *http.Request)
@@ -34,10 +41,13 @@ func handleRoot(p *Proxy, w http.ResponseWriter, r *http.Request) {
     hostComponents := strings.Split(r.Host, ".")
     subdomain := hostComponents[0]
 
+    log.Printf("Received request to proxy for subdomain: %s\n", subdomain)
+
     if proxyValue, ok := p.Clients[subdomain]; ok {
         buf := &bytes.Buffer{}
         r.Write(buf)
         
+        log.Printf("Proxying request\n")
         proxyValue.Conn.WriteMessage(websocket.TextMessage, buf.Bytes())
 
         proxyResponse := <- proxyValue.Channel
@@ -46,7 +56,34 @@ func handleRoot(p *Proxy, w http.ResponseWriter, r *http.Request) {
 }
 
 func handleRegister(p *Proxy, w http.ResponseWriter, r *http.Request) {
-    subdomain := shortuuid.New()
+    var payload RegisterPayload
+
+    err := json.NewDecoder(r.Body).Decode(&payload)
+    if err != nil {
+        log.Printf("Bad client registration\n")
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+
+    subdomain := strings.ToLower(shortuuid.New())
+    log.Printf("Proxy subscriber registered at subdomain: %s\n", subdomain)
+
+    for _, requestedPort := range payload.Ports {
+        foundPort := false
+        for _, activePort := range p.Ports {
+            if requestedPort == activePort {
+                foundPort = true
+                break
+            }
+        }
+
+        if !foundPort {
+            log.Printf("Opening additional port for proxying: %s\n", requestedPort)
+
+            go http.ListenAndServe(fmt.Sprintf(":%s", requestedPort), p.Router)
+            p.Ports = append(p.Ports, requestedPort)
+        }
+    }
 
     p.Clients[subdomain] = &ProxyValue{Conn: nil, Channel: make(chan []byte)}
     fmt.Fprintf(w, subdomain)
@@ -54,6 +91,8 @@ func handleRegister(p *Proxy, w http.ResponseWriter, r *http.Request) {
 
 func handleProxy(p *Proxy, w http.ResponseWriter, r *http.Request) {
     var err error
+
+    log.Printf("Opening proxy websocket connection\n")
 
     hostComponents := strings.Split(r.Host, ".")
     subdomain := hostComponents[0]
@@ -75,19 +114,21 @@ func handleProxy(p *Proxy, w http.ResponseWriter, r *http.Request) {
                 break
             }
     
+            log.Printf("Writing proxy response to channel\n")
             proxyValue.Channel <- message
         }
+    } else {
+        log.Printf("Subdomain not found: %s\n", subdomain)
     }
 }
 
 func main() {
-    proxy := &Proxy{Clients: make(map[string]*ProxyValue)}
-
     r := mux.NewRouter()
 
-    r.HandleFunc("/register", WrapHandler(proxy, handleRegister)).Methods("GET")
-    r.HandleFunc("/proxy", WrapHandler(proxy, handleProxy)).Methods("GET")
+    proxy := &Proxy{Router: r, Clients: make(map[string]*ProxyValue), Ports: []string{"8080"}}
 
+    r.HandleFunc("/register", WrapHandler(proxy, handleRegister)).Methods("POST")
+    r.HandleFunc("/proxy", WrapHandler(proxy, handleProxy)).Methods("GET")
     r.PathPrefix("/").HandlerFunc(WrapHandler(proxy, handleRoot))
 
     http.ListenAndServe(":8080", r)
